ecc294c5867cb2d103c99cde753b4d5f1f466677
Adam Smakulski <adam.smakulski@gmail.com>
2018-09-30 10:19:13 +0200

    zadanie 3

--- loggers/src/main/java/com/infoshareacademy/loggers/workshop03/Log4jLogger.java
+++ loggers/src/main/java/com/infoshareacademy/loggers/workshop03/Log4jLogger.java
@@ -1,5 +1,8 @@
 package com.infoshareacademy.loggers.workshop03;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.util.Random;
 import java.util.function.Supplier;
 
@@ -8,55 +11,56 @@ public class Log4jLogger {
     private static final Random RANDOM_GENERATOR = new Random();
     private static final int NO_ARGS_DEFINED = 0;
     private static final int ENGINES_ARGS_INDEX = 0;
+    private static final Logger LOG = LogManager.getLogger(Log4jLogger.class);
 
     private static int numberOfActiveEngines = 0;
 
     public static void main(final String[] args) {
 
         if (args.length == NO_ARGS_DEFINED) {
-            // LOGGER.("No engines found! Spacecraft grounded.");
+            LOG.fatal("No engines found! Spacecraft grounded.");
             System.exit(-1);
         }
 
         final int numberOfSpacecraftEngines = Integer.parseInt(args[ENGINES_ARGS_INDEX]);
-        // LOGGER.("Starting " + numberOfSpacecraftEngines + " spacecraft engines ...");
+        LOG.info("Starting " + numberOfSpacecraftEngines + " spacecraft engines ...");
 
         for (int engineNumber = 1; engineNumber <= numberOfSpacecraftEngines; engineNumber++) {
             final boolean engineStarted = startEngine(engineNumber, true);
 
             if (engineStarted) {
-                // LOGGER.("Engine " + engineNumber + " started successfully");
+                LOG.info("Engine " + engineNumber + " started successfully");
                 activateFullPower(engineNumber);
             } else {
-                // LOGGER.("Problem with engine " + engineNumber + ", please check again!");
+                LOG.warn("Problem with engine " + engineNumber + ", please check again!");
                 restartEngine(engineNumber);
             }
         }
 
-        // LOGGER.("Number of active engines: " + numberOfActiveEngines + '/' + numberOfSpacecraftEngines);
+        LOG.info("Number of active engines: " + numberOfActiveEngines + '/' + numberOfSpacecraftEngines);
     }
 
     private static boolean startEngine(final int engineNumber, final boolean firstAttempt) {
-        // LOGGER.("Starting engine: " + engineNumber);
+        LOG.debug("Starting engine: " + engineNumber);
 
         final boolean engineStarted = RANDOM_GENERATOR.nextBoolean();
         Supplier<String> engineStatusSupplier = () -> "Engine " + engineNumber + (engineStarted ? "" : " not") +  " started" ;
 
-        // LOGGER.(engineStatusSupplier);
+        LOG.info(engineStatusSupplier);
 
         if (!engineStarted && !firstAttempt) {
-            // LOGGER.("Engine " + engineNumber + " is not working. Mission failed!");
+            LOG.error("Engine {} is not working. Mission failed!", engineNumber);
         }
         return engineStarted;
     }
 
     private static void activateFullPower(final int engineNumber) {
-        // LOGGER.("Activating full power on engine: " + engineNumber);
+        LOG.info("Activating full power on engine: " + engineNumber);
         numberOfActiveEngines++;
     }
 
     private static void restartEngine(final int engineNumber) {
-        // LOGGER.("Restarting engine: " + engineNumber);
+        LOG.info("Restarting engine: " + engineNumber);
         startEngine(engineNumber, false);
     }
 }
