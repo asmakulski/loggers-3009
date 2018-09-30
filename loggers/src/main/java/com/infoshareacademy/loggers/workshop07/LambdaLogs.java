20e44c464cb6e07a44cfae6335817ed27b89e978
Adam Smakulski <adam.smakulski@gmail.com>
2018-09-30 11:02:59 +0200

    zadanie 7

--- loggers/src/main/java/com/infoshareacademy/loggers/workshop07/LambdaLogs.java
+++ loggers/src/main/java/com/infoshareacademy/loggers/workshop07/LambdaLogs.java
@@ -1,21 +1,26 @@
 package com.infoshareacademy.loggers.workshop07;
 
+import com.infoshareacademy.loggers.workshop06.Slf4jLogger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class LambdaLogs {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(Slf4jLogger.class);
+
     public static void main(String[] args) {
 
         final List<Integer> numbers = IntStream.rangeClosed(1, 10).boxed()
-//                LOGGER.debug("Number: " + number)
+                .peek(number -> LOGGER.debug("Number: " + number))
             .filter(number -> number % 2 == 0)
-//                LOGGER.debug("Even: " + number)
+                .peek(number -> LOGGER.debug("Even: " + number))
             .map(number -> number * 10)
-//                LOGGER.debug("Multiplied: " + number)
+                .peek(number -> LOGGER.debug("Multiplied: " + number))
             .collect(Collectors.toList());
-
-//        LOGGER.info("Final numbers: {}", numbers);
+        LOGGER.info("Final numbers: {}", numbers);
     }
 }
